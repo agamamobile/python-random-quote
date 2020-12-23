a079bb1f979e6e8041f8c924620a7fd51d08791f
agamamobile <53582220+agamamobile@users.noreply.github.com>
2020-12-23 02:22:56 -0500

    random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,10 +1,12 @@
-def startthis():
+import random
 
+def startthis():
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-
-  print(quotes)
-
+  last = 13
+  rnd = random.randint(0, last)
+  print(quotes[rnd])
+  
 if __name__== "__main__":
   startthis()
